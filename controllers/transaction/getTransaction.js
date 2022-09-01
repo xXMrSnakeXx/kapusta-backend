59a558eb23a7bb9cce2a48ac17c2f4dd269f2c5e
AnnaKucherenko <annakucherenko31@gmail.com>
2022-09-01 13:14:03 +0300

    getTransaction, добавляет поля в текущий запрос пользователя, изменения в гугл-редирект

--- controllers/transaction/getTransaction.js
+++ controllers/transaction/getTransaction.js
@@ -1,7 +1,13 @@
-const { createError } = require('../../helpers');
-
-const getTransaction = async (req, res, next) => {
-
-}
-
+const { Transaction } = require("../../models/transactions");
+// const { createError } = require('../../helpers');
+const getTransaction = async (req, res) => {
+  const { id: owner } = req.user;
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Transaction.find({ owner }, "", {
+    skip,
+    limit: Number(limit),
+  }).populate("owner", "email ");
+  res.json(result);
+};
 module.exports = getTransaction;
