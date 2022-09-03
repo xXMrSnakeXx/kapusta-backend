e94103c5a36f11180dda889c9a69a9a09cc4b3cf
AnnaKucherenko <annakucherenko31@gmail.com>
2022-09-03 21:00:49 +0300

    перевод фраз ошибок на английский, удаляет лишний код

--- controllers/transaction/setBalance.js
+++ controllers/transaction/setBalance.js
@@ -1,38 +1,42 @@
-const { User } = require('../../models/user');
+const { User } = require("../../models/user");
 
 const setBalance = async (req, res) => {
-    const { balance: commonBalance } = req.body;
-    const { id, email } = req.user;
+  const { balance: commonBalance } = req.body;
+  const { id, email } = req.user;
 
-    const { balance } = await User.findById(id);
+  const { balance } = await User.findById(id);
 
-    if (balance !== null) {
-        res.status(406).json({
-            status: 'Not Acceptable',
-            code: 406,
-            data: {
-                user: {
-                    email,
-                    balance,
-                },
-            },
-        });
-
-        return;
-    }
-
-    const { balance: updBalance } = await User.findByIdAndUpdate(id, { balance: commonBalance }, { new: true });
-
-    res.status(201).json({
-        status: 'balance update successful',
-        code: 201,
-        data: {
-            user: {
-                email,
-                balance: updBalance,
-            },
+  if (balance !== null) {
+    res.status(406).json({
+      status: "Not Acceptable",
+      code: 406,
+      data: {
+        user: {
+          email,
+          balance,
         },
+      },
     });
+
+    return;
+  }
+
+  const { balance: updBalance } = await User.findByIdAndUpdate(
+    id,
+    { balance: commonBalance },
+    { new: true }
+  );
+
+  res.status(201).json({
+    status: "balance update successful",
+    code: 201,
+    data: {
+      user: {
+        email,
+        balance: updBalance,
+      },
+    },
+  });
 };
 
 module.exports = setBalance;
